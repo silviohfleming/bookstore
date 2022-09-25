54bc687c6f1316f6d2d00d487caa79354ce692cd
Silvio Henrique Fleming <silviohfleming@gmail.com>
2022-09-25 15:43:10 -0300

    [feature/exercicio] Exercicio do módulo 16.

--- order/factories.py
+++ order/factories.py
@@ -1,14 +1,12 @@
 import factory
-
 from django.contrib.auth.models import User
-from product.factories import ProductFactory
 
 from order.models import Order
 
 
 class UserFactory(factory.django.DjangoModelFactory):
-    email = factory.Faker('pystr')
-    username = factory.Faker('pystr')
+    email = factory.Faker("pystr")
+    username = factory.Faker("pystr")
 
     class Meta:
         model = User
